f0d922a3122c2a4586984326666844ca50479f88
Denis Koenig <kyoni9d@gmail.com>
2025-11-07 08:55:54 +0300

    style: use CSS Grid for speaker table rows

--- core/static/core/dom.js
+++ core/static/core/dom.js
@@ -5,16 +5,17 @@ export function createSpeakerButton(speaker) {
     const button = document.createElement("button");
 
     button.type = "button";
-    button.className = "list-group-item list-group-item-action";
+    button.className = "speaker-item list-group-item list-group-item-action";
     button.dataset.id = speaker.id;
     button.dataset.name = speaker.full_name;
     button.dataset.timeLimit = speaker.time_limit_seconds;
 
     button.innerHTML = `
-    <span class="speaker-name">${speaker.full_name}</span> —
-    <span class="speaker-topic">${speaker.topic}</span> —
+    <span class="speaker-name">${speaker.full_name}</span>
+    <span class="speaker-topic text-truncate">&laquo;${speaker.topic}&raquo;</span>
     <span class="speaker-time">${formatTime(speaker.time_limit_seconds)}</span>
     <span class="icon-wrapper delete-speaker text-dark">
+        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" stroke="currentColor" stroke-width=".2" viewBox="0 0 16 16"> -->
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
         </svg>
